a0aa025e0b5517c5f62ff896196ebdc7d407baca
pablojuann97 <pablo.juann97@hotmail.com>
2025-06-20 14:09:26 -0300

    Delete android directory

--- android/app/src/main/kotlin/com/example/noite_aqui/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.noite_aqui
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/noiteaqui/meuapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.noiteaqui.meuapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
